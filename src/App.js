7696d50c53cf17a78a75091ba8229ec7aefc0096
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2022-02-12 16:50:47 +0000

    Add theme to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'styles/theme';
+
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+            <>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/shop" element={<Shop />} />
+                </Routes>
+            </>
+        </ThemeProvider>
     );
 };
 
+// Below for testing
 const Home = () => {
     return <div>Homepage</div>;
+};
+
+const Shop = () => {
+    return <div>Shop page</div>;
 };
 
 export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,50 @@
+export const theme = {
+    colors: {
+        red: {
+            light: '#fc5c65',
+            dark: '#eb3b5a',
+        },
+        orange: {
+            light: '#fd9644',
+            dark: '#fa8231',
+        },
+        yellow: {
+            light: '#fed330',
+            dark: '#f7b731',
+        },
+        green: {
+            light: '#26de81',
+            dark: '#20bf6b',
+        },
+        turquoise: {
+            light: '#2bcbba',
+            dark: '#0fb9b1',
+        },
+        blue: {
+            light: '#45aaf2',
+            dark: '#2d98da',
+        },
+        darkBlue: {
+            light: '#4b7bec',
+            dark: '#3867d6',
+        },
+        purple: {
+            light: '#a55eea',
+            dark: '#8854d0',
+        },
+        grey: {
+            light: '#d1d8e0',
+            dark: '#a5b1c2',
+        },
+        darkGrey: {
+            light: '#778ca3',
+            dark: '#4b6584',
+        },
+    },
+    breakPoints: {
+        tablets: '481px',
+        laptops: '769px',
+        desktops: '1025px',
+        largeScreens: '1201px',
+    },
+};
