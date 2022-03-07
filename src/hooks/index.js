78231318311eb7372718f18b3683f7adc15905f5
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2022-03-07 10:40:04 +0000

    Create useShop hook to bring in product data

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useMediaQuery } from './useMediaQuery';
 export { default as useUI } from './useUI';
 export { default as useFetch } from './useFetch';
+export { default as useShop } from './useShop';
---
+++ src/hooks/useShop.js
@@ -0,0 +1,21 @@
+import { useFetch } from 'hooks';
+
+const SHOP_API_URL = 'https://fakestoreapi.com/products';
+const DESIRED_CATEGORIES = ["men's clothing", "women's clothing"];
+
+const useShop = () => {
+    const { data: products, loading, error } = useFetch(SHOP_API_URL);
+
+    // filter products to only show clothing items
+    const filteredProducts = products?.filter((product) =>
+        DESIRED_CATEGORIES.includes(product.category)
+    );
+
+    return {
+        products: filteredProducts,
+        loading,
+        error,
+    };
+};
+
+export default useShop;
