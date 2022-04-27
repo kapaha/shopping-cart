eeb254c0235020b80b45eace1b6ac53f3f34232a
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2022-04-27 11:08:18 +0100

    Add useCart hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export { default as useMediaQuery } from './useMediaQuery';
 export { default as useUI } from './useUI';
 export { default as useFetch } from './useFetch';
 export { default as useShop } from './useShop';
+export { default as useCart } from './useCart';
---
+++ src/hooks/useCart.js
@@ -0,0 +1,72 @@
+import { useState, useRef } from 'react';
+
+const CART_LOADING_DELAY = 1500; // ms
+const CART_READY_DELAY = 4000; // ms
+const CART_STATUS = Object.freeze({
+    READY: 'READY',
+    ADDING_PRODUCT: 'ADDING_PRODUCT',
+    ADDING_PRODUCT_COMPLETE: 'ADDING_PRODUCT_COMPLETE',
+});
+
+const useCart = () => {
+    const [cart, setCart] = useState([]);
+    const [cartStatus, setCartStatus] = useState(CART_STATUS.READY);
+
+    const cartReadyTimer = useRef(null);
+
+    function incCartProductQuantity(product, quantity) {
+        setCart((prevState) => {
+            // check if cart already contains the product
+            const containsProduct = cart.find(
+                (item) => item.title === product.title
+            );
+
+            // if it does increase its quantity
+            if (containsProduct) {
+                return prevState.map((item) =>
+                    item.title === product.title
+                        ? { ...product, quantity: item.quantity + quantity }
+                        : item
+                );
+            }
+
+            // if it doesnt add the item and quantity to the cart
+            return [...prevState, { ...product, quantity }];
+        });
+    }
+
+    function addToCart(product, quantity) {
+        if (cartStatus === CART_STATUS.ADDING_PRODUCT) return;
+
+        clearInterval(cartReadyTimer.current);
+
+        setCartStatus(CART_STATUS.ADDING_PRODUCT);
+
+        setTimeout(() => {
+            incCartProductQuantity(product, quantity);
+
+            setCartStatus(CART_STATUS.ADDING_PRODUCT_COMPLETE);
+
+            cartReadyTimer.current = setTimeout(() => {
+                setCartStatus(CART_STATUS.READY);
+            }, CART_READY_DELAY);
+        }, CART_LOADING_DELAY);
+    }
+
+    function getCartQuantity() {
+        return cart.length === 0
+            ? 0
+            : cart.reduce(
+                  (accumulator, object) => accumulator + object.quantity,
+                  0
+              );
+    }
+
+    return {
+        cartStatus,
+        getCartQuantity,
+        addToCart,
+    };
+};
+
+export default useCart;
