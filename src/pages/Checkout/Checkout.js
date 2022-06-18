014f0479741499366b3def7272d5b281a0645cce
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2022-06-18 14:22:18 +0100

    Change checkout message

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { PageContainer } from 'components';
+import { Bold } from './Checkout.styled';
 
 const Checkout = () => {
-    return <div>Sorry, the checkout page has not been implemented.</div>;
+    return (
+        <PageContainer>
+            <p>
+                <Bold>Checkout is closed.</Bold> This store isn't set up to
+                process payments.
+            </p>
+        </PageContainer>
+    );
 };
 
 export default Checkout;
